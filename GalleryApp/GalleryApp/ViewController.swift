42ffedddf04206c7423dc1efde00690f17fd1947
gcarolina <karolinaganizoda@gmail.com>
2024-01-28 22:16:50 +0300

    Created project

---
+++ GalleryApp/GalleryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GalleryApp
+//
+//  Created by Carolina on 28.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
