218cfd5f7b1b9d47e5c9173ba8286de530f89a6b
gcarolina <karolinaganizoda@gmail.com>
2024-02-06 12:56:22 +0300

    Added screens associated with Image detail

---
+++ GalleryApp/GalleryApp/Views/ImageDetail/ImageDetailViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class ImageDetailViewController: UIViewController {
+    
+    private var collectionView: UICollectionView?
+    private var collectionViewFlowLayout: UICollectionViewFlowLayout?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+  
+}
